747bcca8b5dd6cf0632b2ffccd2b298bde8a7d56
fravitto00 <francesco.vittori89@gmail.com>
2024-10-11 18:57:30 +0200

    Removed cookie consent banner from screenshot

--- src/services/puppeteer.js
+++ src/services/puppeteer.js
@@ -3,10 +3,16 @@ import puppeteer from "puppeteer";
 async function captureTradingViewScreenshot(symbol) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const cookieToPreventBanner = {
+    "name": "cookiePrivacyPreferenceBannerProduction",
+    "value": "accepted",
+    "domain": ".tradingview.com",
+  }
+  await page.setCookie(cookieToPreventBanner);
   await page.goto(`https://it.tradingview.com/chart/GoLIDkHa/?symbol=${symbol}`);
 
   const graphElement = await page.waitForSelector('body > div.js-rootresizer__contents.layout-with-border-radius > div.layout__area--center > div > div.chart-container-border > div > div.chart-markup-table');
-  const screenshot = await graphElement.screenshot({ encoding: 'base64' }); // Use base64 encoding
+  const screenshot = await graphElement.screenshot();
   await browser.close();
   return screenshot;
 }
